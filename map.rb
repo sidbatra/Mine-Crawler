ac0e8e657f88805b5791c2f4ec5efc46c35b0e72
Siddharth Batra <siddharthabatra@gmail.com>
2012-04-16 22:37:38 +0000

    error & crash proofing the mapper.

--- map.rb
+++ map.rb
@@ -4,43 +4,103 @@ require 'rubygems'
 require 'nokogiri'
 require 'fastimage'
 
-#STDIN.each_line do |line|
-#  url,html=line.split("\t")
-#
-#  next unless url && url.length && html && html.length
-#  puts url if(html.match("og:title"))
-#end
+#url = "http://www.jcrew.com/AST/Navigation/Shoes/men/PRDOVR~28357/28357.jsp"
+#html =`cat data`
 
-data =`cat data`
-doc = Nokogiri::HTML(data)
 
-doc.xpath("//meta[@property='og:title']/@content").each do |node|
-  puts node.value
+STDIN.each_line do |line|
+  url,html=line.split("\t")
+
+  begin
+
+  next if url.nil? || url.length.zero? || html.nil? || html.length.zero?
+
+  uri = URI.parse(URI.encode(url))
+  title = ""
+  description = ""
+  images = []
+  image = ""
+  image_size = [0,0]
+
+  next unless html.match("og:title")
+
+
+  doc = Nokogiri::HTML(html)
+
+  next if doc.nil?
+
+
+  ##
+  # Title
+  ##
+  doc.xpath("//meta[@property='og:title']/@content").each do |node|
+    title = node.value if node.value
+  end
+
+  doc.xpath("//title").each do |node|
+    title = node.children.text if node.children && node.children.text
+  end if title.length.zero?
+
+  doc.xpath("//meta[@name='title']/@content").each do |node|
+    title = node.value if node.value
+  end if title.length.zero?
+
+
+  ##
+  # Description
+  ##
+  doc.xpath("//meta[@property='og:description']/@content").each do |node|
+    description = node.value if node.value
+  end
+
+  doc.xpath("//meta[@name='description']/@content").each do |node|
+    description = node.value if node.value
+  end if description.length.zero?
+
+
+  ##
+  # Image
+  ##
+  doc.xpath("//meta[@property='og:image']/@content").each do |node|
+    images << node.value if node.value
+  end
+
+  doc.xpath("//img").each do |img|
+    src = img['src']
+    next if src.nil? || src.length.zero?
+
+    begin
+      images << (src.match(/^http/) ? 
+                  src : 
+                  URI.decode(uri.merge(URI.encode(src)).to_s).gsub("¥","\\"))
+    rescue
+      puts "Error parsing image src - #{src}"
+    end
+  end
+
+  images.each do |img|
+    begin
+      size = FastImage.size(img)
+    rescue
+      puts "Error fetching img - #{img}"
+    end
+
+    if size && size[0] > image_size[0] && size[1] > image_size[1]
+      image = img 
+      image_size = size
+    end
+  end
+
+
+  puts "Url: " + url
+  puts "Tilte: " + title
+  puts "Description: " + description
+  puts "Image: " + image
+  puts "Image Size: " + image_size.join(",")
+  puts ""
+
+  rescue
+    puts "Error with url - #{url}"
+  end
+
 end
-#puts doc.css("meta[property='og:title']").first.attributes['content'].value
-
-doc.xpath("//meta[@property='og:image']/@content").each do |node|
-  puts node.value
-end
-#puts doc.css("meta[property='og:image']").first.attributes['content'].value
-
-doc.xpath("//title").each do |node|
-  puts node.children.text
-end
-#puts doc.css('title').children.text
-
-
-
-
-puts "\n\nIMAGES \n"
-
-#TODO: Multi thread
-#
-doc.xpath("//img").each do |img|
-  p img['src'], FastImage.size(img['src'])
-end
-#doc.css("img").each do |img|
-#  puts img['src']#, FastImage.size(img['src'])
-#end
-
-
