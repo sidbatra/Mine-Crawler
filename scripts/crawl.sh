eae363f58274124554a985f817fe6b64bffda9e7
Siddharth Batra <siddharthabatra@gmail.com>
2012-04-21 02:19:29 +0000

    porting crawldb to and from s3

--- scripts/crawl.sh
+++ scripts/crawl.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+hadoop distcp s3n://denwen-mine-crawls/crawl /crawl
+
 bin/nutch crawl s3n://denwen-mine-crawls/1334711638/urls -depth 12 -topN 100000 -dir /crawl -threads 40 &> crawl.txt 
 bin/nutch readseg -dump /crawl/segments/*  /output  -nogenerate -noparse -noparsedata -noparsetext &> readseg.txt
 hive -f "hive.sql" &> hive.txt
 
+# Create backup of current crawl on s3
+#
+hadoop distcp -overwrite /crawl s3n://denwen-mine-crawls/crawl
 
