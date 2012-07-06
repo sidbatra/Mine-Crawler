ffa4f580055c585e6c053cb2360eebca0f64408a
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-06 00:20:07 +0000

    basic framework for crawler daemon.

--- lib/daemons/crawler.rb
+++ lib/daemons/crawler.rb
@@ -5,10 +5,23 @@ ROOT_PATH = File.dirname(__FILE__) + "/../../"
 
 
 require 'rubygems'
-require 'json'
 require 'logger'
+require 'aws'
+require 'json'
+require 'yaml'
+
+require File.join(ROOT_PATH,"external/fifo/lib/fifo.rb")
+
 
 @logger = Logger.new(File.join(ROOT_PATH,"log/crawler.rb.log"))
+@env = ENV['RAILS_ENV']
+
+CONFIG = YAML.load_file(File.join(ROOT_PATH,"config/config.yml"))[@env]
+
+FIFO::QueueManager.setup(CONFIG[:aws_access_id],CONFIG[:aws_secret_key])
+ProcessingQueue = FIFO::Queue.new [@env,CONFIG[:queue][:proc]].join("_")
+CrawlQueue = FIFO::Queue.new [@env,CONFIG[:queue][:crawl]].join("_")
+
 
 
 class Crawler
@@ -16,7 +29,9 @@ class Crawler
   def self.start(json)
     stores = JSON.parse json
   end
+
 end
+
 
 
 $running = true
@@ -24,30 +39,29 @@ Signal.trap("TERM") do
   $running = false
 end
 
-puts "hello"
-#while($running) do
-#  payload = CrawlQueue.pop
-#
-#  if payload
-#
-#    begin
-#      start_time = Time.now
-#      payload.process
-#      end_time = Time.now
-#
-#      @logger.info "Finished #{payload.to_s} #{end_time - start_time}"
-#
-#    rescue => ex
-#      payload.failed
-#
-#      if payload.attempts < 3
-#        @logger.info "Recovering #{payload.to_s}"
-#        payload.queue.push(payload)
-#      end
-#    end
-#
-#  end 
-#  
-#  sleep 3
-#end
+while($running) do
+  payload = CrawlQueue.pop
+
+  if payload
+
+    begin
+      start_time = Time.now
+      payload.process
+      end_time = Time.now
+
+      @logger.info "Finished #{payload.to_s} #{end_time - start_time}"
+
+    rescue => ex
+      payload.failed
+
+      if payload.attempts < 3
+        @logger.info "Recovering #{payload.to_s}"
+        payload.queue.push(payload)
+      end
+    end
+
+  end 
+  
+  sleep 3
+end
 
